23dba94279445a23c72c9ffb07444a6f947ac940
skerbos <79687493+skerbos@users.noreply.github.com>
2022-03-23 13:11:17 +0800

    Past Project: Shoot2Move
    A shooter game inspired from asteroids

---
+++ Shoot2Move/shoot2move/Assets/Scripts/Enemies/Enemy3.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3 : MonoBehaviour
+{
+    public GameObject enemyBullet;
+    public int bulletsPerShot = 5;
+    public EnemyClasses.BasicEnemy spinShooter = new EnemyClasses.BasicEnemy(20f, 2f, 10f, 0.5f, 5f, 10f, 0);
+    public GameObject deathParticles;
+    public AudioSource deathSound;
+    private Rigidbody2D rb;
+    private GameObject playerGun;
+    private GameObject playerCamera;
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        playerGun = GameObject.FindGameObjectWithTag("PlayerGun");
+        playerCamera = GameObject.FindGameObjectWithTag("CameraHolder");
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootRaidally();
+        Spin();
+    }
+
+    void ShootRaidally()
+    {
+        if (Time.time > nextFire)
+        {
+            for (int i = 1; i <= bulletsPerShot; i++)
+            {
+                Instantiate(enemyBullet, transform.position, transform.rotation * Quaternion.Euler(0, 0, i*(360f/bulletsPerShot)));
+            }
+            nextFire = Time.time + 1.5f;
+        }
+    }
+
+    void Spin()
+    {
+        rb.angularVelocity = 20f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerBullet"))
+        {
+            spinShooter.TakeDamage(playerGun);
+            spinShooter.Death(gameObject, deathParticles, deathSound, playerCamera);
+        }
+
+    }
+}
---
+++ Shoot2Move/shoot2move/Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerControl : MonoBehaviour
+{
+       
+    private Vector2 mousePos;
+    private Vector2 gunPos;
+    private Rigidbody2D rb;
+    private GameObject playerCamera;
+    private float iFramesTime = 0.5f;
+    private bool isAlive = true;
+    public GameObject gun;
+    public GameObject bullet;
+    public GameObject playerDamageParticles;
+    public GameObject lockSprite;
+    public bool positionLock; 
+    public float maxSpeed = 1000f;
+    public float dirAngle;
+    public int hitsRemaining = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = GameObject.FindGameObjectWithTag("CameraHolder");
+        rb = transform.GetComponent<Rigidbody2D>();
+        positionLock = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseLook();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            LockPosition();
+            lockSprite.SetActive(true);
+        }
+        else
+        {
+            LimitMaxSpeed();
+            lockSprite.SetActive(false);
+        }
+    }
+
+    void MouseLook()
+    {
+        mousePos = Input.mousePosition;
+        gunPos = Camera.main.WorldToScreenPoint(gun.transform.position);
+        Vector3 aimDir = mousePos - gunPos;
+        dirAngle = Mathf.Atan2(aimDir.y,aimDir.x) * Mathf.Rad2Deg;
+        gun.transform.rotation = Quaternion.Euler(0,0,dirAngle);
+    }
+
+    void LimitMaxSpeed()
+    {
+        positionLock = false;
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+
+    void LockPosition()
+    {
+        positionLock = true;
+        rb.velocity = rb.velocity.normalized * 0;
+    }
+
+    void isDamaged()
+    {
+        playerCamera.GetComponent<CameraControl>().cameraShake(0.1f, 1f);
+        GameObject playerDeathParticlesClone = Instantiate(playerDamageParticles, transform);
+        Destroy(playerDeathParticlesClone, 0.2f);
+        hitsRemaining -= 1;
+        if (hitsRemaining <= 0)
+        {
+            gameObject.SetActive(false);
+            isAlive = false;
+            SceneManager.LoadScene("death");
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.AddForce(collision.contacts[0].normal * 500f);
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("EnemyBullet"))
+        {
+            isDamaged();
+        }
+    }
+}
